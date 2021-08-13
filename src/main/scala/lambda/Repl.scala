6ed816004cd8cc82769dc2b9b06a945ec882ae2f
Tobias Kolditz <tbs.kldtz@gmail.com>
2021-08-13 16:09:23 +0200

    Add stdlib

--- src/main/scala/lambda/Repl.scala
+++ src/main/scala/lambda/Repl.scala
@@ -2,21 +2,34 @@ package lambda
 
 import scala.collection.mutable
 import scala.collection.mutable.HashMap
+import scala.io.Source
 import scala.io.StdIn.readLine
+import scala.sys.error
 
 object Repl:
   val Assignment = raw"([a-zA-Z_\-]+)\s*=\s*(.+)".r
 
   def run(): Unit =
     var ctx: mutable.AbstractMap[String, Expression] = HashMap()
+    ctx ++= loadSource("stdlib.txt")
     while true do
       print("> ")
-      val line = readLine()
-      val res = line match {
+      val res = readLine() match {
         case "exit" => return
         case line => safeEval(line, ctx)
       }
       println(s"$res")
+
+  private def loadSource(filename: String): Iterator[(String, Expression)] =
+    Source.fromResource(filename)
+      .getLines()
+      .filter(l => !l.startsWith("#"))
+      .map(readAssignment)
+
+  private def readAssignment(line: String): (String, Expression) = line match {
+    case Assignment(v, e) => v -> Interpreter.eval(e)
+    case l => error(s"Could not read source line '$l'")
+  }
 
   private def safeEval(line: String, ctx: mutable.AbstractMap[String, Expression]): String =
     try
